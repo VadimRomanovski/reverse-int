5deb5465bfb68d609aaeb2817aae37dfecf2e16a
VadimRomanovski <49916251+VadimRomanovski@users.noreply.github.com>
2020-03-17 15:02:28 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if(n<0){
+        let a = n*-1;
+        return +a.toString().split('').reverse().join('') 
+     }else{
+         return +n.toString().split('').reverse().join('') 
+     }
 }
